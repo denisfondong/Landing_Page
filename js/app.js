cd7b227fdc9111612d5662ba2bbe649998464c0b
Denis Fondong <95001506+denisfondong@users.noreply.github.com>
2022-05-09 20:49:02 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,51 +6,61 @@ let sections = document.querySelectorAll("section");
 let navBar = document.querySelector("navbar__menu");
 
 // build the nav
-function navBuilder (){
-  sections.forEach(section=> {
-  const itemList = document.createElement("li");
-	itemList.insertAdjacentHTML("afterbegin",`<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
-  
-  navBarList.appendChild(itemList);
-  scrollBehavior(itemList, section);	
-	
-});
-navBar.appendChild(navBarList);
+
+function navBuilder() {
+  sections.forEach(section => {
+    const itemList = document.createElement("li");
+    itemList.insertAdjacentHTML("afterbegin", `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
+
+    navBarList.appendChild(itemList);
+  });
 }
 navBuilder();
 
-
 // Add class 'active' to section when near top of viewport
-function activeSection (){
-  // Select all anchor using "menu__link" class
-  const currentNavBar = document.querySelectorAll(".menu__link")
-  sections.forEach((section, i)=>{
-      //Get the boundingrect for each section 
-      const bounding = section.getBoundingClientRect();
-      //Check if the section is in viewport or not 
-      if (bounding.top <= 380 && bounding.bottom >= 350){
-          //section in viewport accourding to top and bottom boundings
-          section.classList.add("your-active-class");
-          currentNavBar[i].classList.add("active");
-      } else{
-          //When section is off sight
-          section.classList.remove("your-active-class");
-          currentNavBar[i].classList.remove("active");
-      }
-  })
-}
+const addActive = function (conditional, section) {
+  if (conditional) {
+    section.classList.add("your-active-class");
+    //change style of active section
+    section.style.cssText = "background-color:indigo;";
+  };
+};
+const removeActive = function (section) {
+  section.classList.remove("your-active-class");
+  //remove box shadow from inactive section 
+  section.style.cssText = "background-color:linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+};
+//Calculate offset of the section.
+const offset = function (section) {
+  return Math.floor(section.getBoundingClientRect().top);
+};
+
+const sectionActivation = function () {
+  sections.forEach(section => {
+    const elementOffset = offset(section);
+
+    inviewport = () => elementOffset < 380 && elementOffset >= -350;
+
+    removeActive(section);
+    addActive(inviewport(), section);
+  });
+};
+window.addEventListener("scroll", sectionActivation);
+
 // Scroll to anchor ID using scrollTO event
-function scrollBehavior(itemList, section){
-  itemList.addEventListener('click', function(e){
-        e.preventDefault();
-        window.scrollTo({
-            top: section.offsetTop,
-            behavior:"smooth"
-        });
+const scrollBehavior = () => {
+  //Select all the links
+  const links = document.querySelectorAll("navbar__menu a");
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      for (i = 0; i < sections; i++) {
+        section[i].addEventListener("click", sectionScroll(link));
+      }
     });
-}
+  });
+};
+scrollBehavior();
 // Scroll to section on link click
-window.addEventListener('scroll',(e)=>{
-  activeSection();
-  toggleNavBar();
+window.addEventListener("scroll", (e) => {
+  addActive();
 });
